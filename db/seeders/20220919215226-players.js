f74d0a5727de95174f4698b64a2d339db12785e3
Jobson Tenório do Nascimento <jobsontenorio@gmail.com>
2022-09-20 09:11:23 -0300

    Adicionado o seeder Players

--- db/seeders/20220919215226-players.js
+++ db/seeders/20220919215226-players.js
@@ -1,24 +1,38 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
+const filename = path.join(process.cwd(), 'db/seeders/students01.json');
+const students = JSON.parse(fs.readFileSync(filename));
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
      * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
+     await queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
     */
+    
+    const players = students.map((s) => {
+      return {
+        nick: s.email.split('@')[0],
+        name: s.nome,
+        email: s.email,
+      }
+    });
+    await queryInterface.bulkInsert('Players', players, {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Players', null, {});
   }
 };
